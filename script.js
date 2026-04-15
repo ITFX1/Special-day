58700c3a1d574856e49f4c0bc2a6e5aa472284e5
ITFX1 <innovationtrader1@gmail.com>
2026-04-15 15:17:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -467,3 +467,7 @@ Happy Birthday ✨
     music.src = "https://res.cloudinary.com/dn0250gby/video/upload/v1776197883/Benson_Boone_-_Beautiful_Things___Piano_Cover_by_Pianella_Piano_256k_acywfr.mp3";
     music.play().catch(()=>{});
 }
+function goHome() {
+    stopMusic();
+    showSection("home");
+}
