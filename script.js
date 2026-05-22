88e4bd5276e10a0b91ef24327bafaaaf1c211c51
ITFX1 <innovationtrader1@gmail.com>
2026-05-22 22:44:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -27,11 +27,11 @@ let chapters = [ /* 🔥 KEEP ALL YOUR DATA EXACTLY AS IT IS */
 title: "Chapter 1: The First Time I Saw You ❤️✨",
 text: `Hari igihe ubuzima butangira bucece… 🤍 nta rusaku, nta muntu ubimenye, ariko bukagenda bwubaka inkuru idasanzwe 🌙✨
 Ni uko byatangiye kuri njye… igihe nakubonaga bwa mbere 👀❤️
-Hari muri S2 📚 ku kigo cya GS Kiraro Protestant 🏫. Wari uje mushya, uvuye ku kigo cya ES Sumba 🔄. Kuva ku munsi wa mbere, wari utandukanye n’abandi bose ✨. Si uko wivugaga cyane 🤍, ahubwo wari ufite ituze ryihariye ryakurangaga 🌿.
-Nibwo natangiye kugushakaho amakuru 🔍… nshaka kumenya uwo uri we, aho uturuka, n’ibikuranga 💭. Kandi uko nagendaga mbimenya, niko nagendaga mbona ko uri umuntu udasanzwe ✨❤️.
-Wari umukobwa witonda cyane 🤍… ufite isuku idasanzwe ✨, ubona ko witaho mu buryo burenze uko abandi babigenzaga 🌸. Wari smart 📚… buri gihe wambaye neza 👗, wambaye uniform isa neza nk’iyatewe ipasi neza 🧺✨, ibintu byose bikuranga byari byuzuye ituze n’ubwiza bworoheje ariko bufite agaciro 💎.
-Amaso yawe yari afite ubwiza bwihariye 👀✨… atuje 🤍, ariko afite ubusobanuro bwinshi 💭. Nari nkubona ugendana na Lucie 👭, inshuti yawe, ariko n’iyo mwabaga muri kumwe, hari ikintu kuri wowe cyantandukaniraga n’abandi bose ✨.
-Sinari nzi neza icyo ari cyo… ariko nari nzi neza ko hari ikintu cyantangiye gukurura umutima wanjye 💓 bucece 🤍.`,
+Hari muri S2 📚 ku kigo Nigagaho cya GS Kiraro Protestant 🏫. Wari uje uri mushya, uvuye ku kigo cya ES Sumba 🔄. Kuva ku munsi wa mbere, wari utandukanye n’abandi bose ✨. Si uko wivugaga cyane 🤍, ahubwo wari ufite ituze ryihariye ryakurangaga 🌿.
+Nibwo natangiye kugushakaho amakuru 🔍… nshaka kumenya uwo uri we, aho uturuka, n’ibikuranga 💭. Kandi uko nagendaga mbimenya, niko nagendaga mbona ko uri umuntu udasanzwe kuri nge ✨❤️.
+Wari umukobwa witonda cyane 🤍👌🏾… ufite isuku idasanzwe ✨, ubona ko wiyitaho mu buryo burenze uko abandi babigenzaga 🌸. Wari smart 👌🏾📚… buri gihe wambaye neza 👗, wambaye uniform isa neza nk’iyatewe ipasi neza 🧺✨, ibintu byose byakuranga byari byuzuye ituze n’ubwiza bworoheje ariko bufite agaciro 💎.
+Amaso yawe yari afite ubwiza bwihariye 👀✨… atuje 🤍, ariko afite ubusobanuro bwinshi 💭. Nakubonaga ukunda kugendana na Lucie 👭, inshuti yawe, ariko n’iyo mwabaga muri kumwe, hari ikintu kuri wowe cyantandukaniraga n’abandi bose ✨.
+Sinari nzi neza icyo ari cyo… ariko nari nzi neza ko hari ikintu cyatangiye gukurura umutima wanjye 💓 bucece 🤍.`,
 bg: "https://res.cloudinary.com/dn0250gby/image/upload/v1775893735/img1_gc7cje.jpg",
 song: "https://res.cloudinary.com/dn0250gby/video/upload/v1775897248/song1_wxtwek.mp3"
 
@@ -40,14 +40,14 @@ song: "https://res.cloudinary.com/dn0250gby/video/upload/v1775897248/song1_wxtwe
 {
 title: "Chapter 2: The Silent Connection 🤍📖✨",
 text: `Nubwo twahuriraga ku kigo buri munsi… 🏫👀 ntabwo twavuganaga 🤐
-Si uko tutabishakaga… ahubwo amasoni yari igice cyacu twembi 😊🤍. Njye nkiri umuntu utari usanzwe uvugana n’abakobwa 😅, nawe ufite ituze n’isoni byari bikuranga 🌸. Twabaga turi hafi… ariko kure mu magambo 💭💔.
+Si uko tutabishakaga… ahubwo amasoni😂 yari igice cyacu twembi 😊🤍. Njye nari umuntu utari usanzwe uvugana n’abakobwa 😅, nawe ufite ituze n’isoni byari bikuranga 🌸. Twabaga turi hafi… ariko kure mu magambo 💭💔.
 Ariko ubuzima bugira uburyo bwabwo bwo guhuza abantu 🌙✨.
-Umunsi umwe, nabonye ifoto yawe kuri Facebook 📱👀. Wari uherutse gufungura account yawe… kandi ako kanya nahise menya ko ari wowe ❤️. Ifoto yawe wari wambaye jipo y’igitengi 👗, wambaye agapira k’amaboko maremare gafite ijosi 🤍, ufite n’agakapu gato muntoki 🎒✨. Byari byoroshye… ariko byari byiza cyane 💎❤️.
-Nta gutekereza kabiri… nahise ngusaba friendship 🤝📩. Kandi igihe wabinyemereye, ni bwo inkuru yacu yatangiye mu buryo butari bwitezwe 🌙✨.
+Umunsi umwe, nabonye ifoto yawe kuri Facebook 📱👀. Wari uherutse gufungura account yawe… kandi ako kanya nahise menya ko ari wowe ❤️. Ifoto yawe wari wambaye ijipo y’igitengi 👗, wambaye agapira k’amaboko maremare gafite ijosi 🤍, ufite n’agakapu gato muntoki 👜✨. Byari byoroshye… ariko byari byiza cyane 💎❤️.
+Nta gutekereza kabiri… nahise ngusaba friendship 🤝📩. Kandi igihe wabinyemereye, ni bwo inkuru yacu yatangiye mu buryo butari bwitezwe👌🏾 🌙✨.
 Twatangiye kuganira… 💬 mu magambo asanzwe ya buri munsi. “Hi 👋”, “amakuru 😊”, n’ibindi byo kumenyana 🤍. Nta muntu wari uzi aho byaganaga… ariko buhoro buhoro, twatangiye kumenyana mu buryo bwimbitse 💭❤️.
 Igihe cyageze turi muri S3 📚✨… maze ibintu bitangira guhinduka buhoro buhoro 🔄. By’umwihariko mu gihe cya COVID-19 🦠🌍, igihe isi yari ihagaze gato ⏸️, ariko ibiganiro byacu byo byari biri gukura 📈❤️. Ni bwo bwa mbere romantic vibes zatangiye kugaragara hagati yacu 💓✨.
-Ariko n’iyo twari tumaze kwegerana mu mutima 💖… ku kigo byari bikiri bya bindi 🤐🏫. Amasoni 😅, kurebana gusa 👀, guca hafi y’undi utavuze 🚶🏾‍♂️🚶🏾‍♀️… ariko umutima wo wari waramaze gutangira kuvuga 💗.
-Ndacyibuka neza… igihe cyose nakubonaga 👀❤️, n’iyo tutavugaga 🤍, hari ikintu cyahindukaga muri njye 💫. Umutima wanjye warihutaga gutera 💓💓… numva ntakimeze nk’uko cyari gisanzwe 🌙.
+Ariko n’iyo twari tumaze kwegerana mu mutima 💖… ku kigo byari bikiri bya bindi 🤐🏫. Amasoni 😅, kurebana gusa 👀, guca hafi y’undi utavuze 🚶🏾‍♂️🚶🏾‍♀️… ariko umutima wo wari waramaze gutangira kuvuga 🗣️💗.
+Ndacyibuka neza… igihe cyose nakubonaga 👀❤️, n’iyo tutavugaga 🤍, hari ikintu cyahindukaga muri njye 💫. Umutima wanjye warihutaga gutera 💓💓… nkumva ntakimeze nk’uko nari nsanzwe 🌙.
 Ni bwo natangiye kumva ko ibyo nari mfite… atari ibisanzwe ❤️‍🔥.
 Ko hari ikintu cyari kiri gukura… 🌱💖 kandi cyari gikomeye kurusha uko nari narigeze kubitekereza 😶‍🌫️✨.`,
 bg: "https://res.cloudinary.com/dn0250gby/image/upload/v1775893734/img2_durf24.jpg",
@@ -60,15 +60,15 @@ text: `
 Hari igihe umutima uba waramaze gufata umwanzuro… 💭❤️ nubwo amagambo ataravuga 🤐🌙
 Kuri twe, urukundo rwari rwaratangiye kera… 🤍 mu buryo butuje 🌿, mu biganiro bya buri munsi 💬, mu buryo bwo kumenyana buhoro buhoro 👀✨. Icyari gisigaye, kwari ukubyemera no kubivuga 🫶❤️.
 Kandi uwo mwanya warageze 🌙✨.
-Ni wowe wabimbwiye mbere… 💌 nubwo mbere yaho naguhatiraga kubyemera ukabyanga 😅🤍, wenda ugerageza guhisha uko wiyumvaga 💭. Ariko igihe cyarageze umutima wawe uravuga 💓, umbwira ukuri ✨.
-Njye… sinahise mbigusubiza ako kanya 🤐💭. Si uko ntabyumvaga… ahubwo ni uko nari narifatiye icyemezo cyanjye 🧠✨. Nari naribwiye ko nzakunda umukobwa ari uko ngeze igihe cyo gushaka 🕰️🤍.
+Ni wowe wabimbwiye mbere… 💌😂 nubwo nyuma yaho naguhatiraga kubyemera ukabyanga 😅🤍, wenda ugerageza guhisha uko wiyumvaga 💭. Ariko igihe cyarageze umutima wawe uravuga 💓, umbwira ukuri ✨.
+Njye… sinahise mbigusubiza ako kanya 🤐💭. Si uko ntabyumvaga… ahubwo ni uko nari narifatiye icyemezo cyanjye 🧠✨. Nari naribwiye ko nzakunda umukobwa ari uko ngeze igihe cyo gushaka📝 🕰️🤍.
 Ariko wowe… waje uhindura ibyo natekerezaga byose 🔄❤️.
-Ijoro ryose naryaga nkabura amahoro 🌙😔, nkibaza 💭, nitekerezaho 😶‍🌫️, nitekereza kuri wowe ❤️… kugeza ubwo nanjye nemeye ukuri ✨.
+Ijoro ryose nararyamaga nkabura amahoro 🌙😔, nkibaza 💭, nitekerezaho 😶‍🌫️, nitekerereza kuri wowe ❤️… kugeza ubwo nanjye nemeye ukuri ✨.
 Ko ibyo nari mfite kuri wowe… atari ibintu byo gutinza cyangwa guhisha 🤍💔.
 Ni bwo urukundo rwacu rwatangiye mu buryo bweruye ❤️‍🔥✨.
 Byari byiza… 💖 birenze uko nabisobanura ✍️. Twari dufite ibyishimo byoroshye 😊 ariko byuzuye 💞.
-Twasezeranye ibintu byinshi… 🤝 tutazibagirana, kutazigera duhemukirana 💍🤍. Twavugaga ejo hazaza twuzuye icyizere 🌅✨—wowe ukaba mama w’abana banjye 👩‍👧‍👦❤️, nanjye nkaba papa w’abana bawe 👨‍👧‍👦💙.
-Nubwo byari amagambo asa n’ayo mu bwana 😅💭… kuri njye yari yuzuyemo ukuri ❤️✨.
+Twasezeranye ibintu byinshi… 🤝 kutazibagirana, kutazigera duhemukirana 💍🤍 😭. Twavugaga ejo hazaza twuzuye icyizere 🌅✨—wowe ukaba mama w’abana banjye 👩‍👧‍👦❤️, nanjye nkaba papa w’abana bawe 👨‍👧‍👦💙.
+Nubwo byari amagambo asa n’ayo mu bwana 😅💭… kuri njye yari yuzuyemo ukuri 💯/💯 ❤️✨.
 Kandi muri icyo gihe cyose… numvaga mfite umuntu wihariye 💖, umuntu wanjye by’ukuri 🤍✨.`,
 bg: "https://res.cloudinary.com/dn0250gby/image/upload/v1775893726/img3_rgrsoj.jpg",
 song: "https://res.cloudinary.com/dn0250gby/video/upload/v1775893824/song3_dycghp.mp3"
@@ -77,25 +77,25 @@ song: "https://res.cloudinary.com/dn0250gby/video/upload/v1775893824/song3_dycgh
 {
 title: "Chapter 4: The Distance I Never Explained 💔🌙📖",
 text: `
-Ariko ubuzima ntibuhora bugenda uko tubwifuza… 🌪️🤍
+Ariko ubuzima ntibuhora bugenda uko tubwifuza…😞 🌪️🤍
 Hari igihe ibintu bihinduka buhoro buhoro… 🔄 nta nubwo ubimenya neza 💭.
 Ni ko byatangiye kuri njye… 😔
-Igihe twari tugeze muri Advanced Level 📚✨, twari dutangiye guhura n’ubuzima busaba byinshi kurushaho 💼💔. Iyo twajyaga mu biruhuko 🏠, wanyandikiraga 📩… nanjye nkagusubiza 💬, ariko si uko byahoze 🤍. Ibiganiro byacu byatangiye kugabanuka 📉, romantic vibes zitangira kuzima buhoro buhoro 💔✨.
-Si uko naguhinduye… 😔 si uko nakunze undi ❌❤️.
-Ahubwo ni uko ubuzima bwanjye bwari bumeze mu buryo utari ubona neza 👤🌫️.
-Hari igihe nabaga nta telephone mfite 📵. Kugira ngo nkwandikire 📩, byasabaga gutira iya mama 🤲📱… kandi hari igihe nabaga ntayibonye 😔.
+Igihe twari tugeze muri Advanced Level 📚✨, twari dutangiye guhura n’ubuzima busaba byinshi kurushaho 💼💔. Iyo twazaga mu biruhuko 🏠, wanyandikiraga 📩… nanjye nkagusubiza 💬, ariko si uko byahoze 🤍. Ibiganiro byacu byatangiye kugabanuka 📉, romantic vibes zitangira kuzima buhoro buhoro 💔✨.
+Si uko nari naraguhinduye… 😔 si uko nari narakunze undi ❌❤️.
+Ahubwo ni uko ubuzima bwanjye bwari bumeze mu buryo utabashaga kubona neza 👤🌫️.
+Nize Advanced level yose nta telephone mfite 📵. Kugira ngo nkwandikire 📩, byasabaga gutira iya mama 🤲📱… kandi hari igihe akenshi nabaga ntayibonye 😔.
 Ariko hari n’ibirenze ibyo 💔…
-Hari ibintu nari ntwaye njyenyine 🤍🧠… inshingano z’ubuzima ziremereye ku mutima w’umuntu ukiri muto 🌧️. Mama yari arwaye 🤍🩹, afite ubumuga… papa atari hafi 🚶‍♂️❌. Byageze aho ubuzima bunshyiraho inshingano zo kuba njyewe nita ku rugo 🏠💔, njyenyine.
+Hari ibintu nari ntwaye njyenyine 🤍🧠… inshingano z’ubuzima ziremereye ku mutima w’umuntu ukiri muto 🌧️. Mama yahoraga arwaye 🤍🩹 nawe urabizi😭, afite ubumuga… papa atari hafi 🚶‍♂️❌😭. Byageze aho ubuzima bunshyiraho inshingano zo kuba njyewe mugihe abavandimwe bange bose bari batakiba murugo kubera inshingano zingo zabo bagombaga kuzuza ubwo bikaba ngomba ko nita ku rugo 🏠💔, njyenyine.
 Mu gihe abandi batekerezaga urukundo… ❤️🌙 njye narimo ngerageza kubaho 💪😔 no kubaka ejo hazaza 🌅, ntari nzi neza aho bizangeza.
 Ariko ikibazo si ibyo nakoze… ❌
 Ikibazo ni uko ntabigusobanuriye 🤍💭.
-Wowe wabonaga gusa ko ndi kure… 😔 ko ntakiguha umwanya 📉, ko wenda hari abandi naguhisemoho 💔❓.
+Wowe wabonaga gusa ko ndi kure… 😔 ko ntakiguha umwanya 📉, ko wenda hari abandi naguhisimbuzaga 💔❓.
 Kandi uko byagendaga bityo… 🔄 ni ko wagiye ucika intege 💔📉.
 Ntabwo nagushinja ❌🤍. Oya.
 Wari ufite uburenganzira bwo kumva ko utitaweho 😔. Wari ukwiye kubona umuntu uguha umwanya ⏳❤️, ugaragaza ko agukunda mu buryo bwumvikana 💬💖.
 Ariko ukuri ni uku… 🌙
 Mu gihe watekerezaga ko ngenda nkwibagirwa… 💭💔
-Njye narimo ngerageza kubaho 😔💪, nkomeza umutima ❤️‍🩹, ngerageza kubaka ubuzima nari nzi ko umunsi umwe nashakaga gusangiza nawe 🌅🤍.
+Njye narimo ngerageza kubaho 😔💪, nkomeza umutima ❤️‍🩹, ngerageza kubaka ubuzima nari nzi ko umunsi umwe nashakaga gusangiza nawe 🌅🤍 Tukabana nkuko twari twarabyiyemeje😭.
 Ariko icyo gihe… sinabikoze neza 💔.
 Kandi nzi neza ko ibyo byakubabaje 😔🌧️.`,
 bg: "https://res.cloudinary.com/dn0250gby/image/upload/v1775893729/img4_dxty6w.jpg",
@@ -105,7 +105,7 @@ song: "https://res.cloudinary.com/dn0250gby/video/upload/v1775897336/song4_uvp5j
 {
 title: "Chapter 5: The Last Words That Stayed 💔🌙📖",
 text: `
-Hari amagambo amwe atajya asibangana… 🕰️💭 n’iyo imyaka yagenda, n’iyo ibintu bihinduka 🔄🤍.
+Hari amagambo amwe atajya asibangana… 🕰️💭 n’iyo imyaka yagenda, n’iyo ibintu byahinduka 🔄🤍.
 Njye mfite amagambo yawe nkiri kumva kugeza n’uyu munsi 🎧💔.
 Byari ku itariki ya 3/9/2022 📅 saa 10:10 z’umugoroba 🌙⏰… ubwo wanyandikiraga kuri Facebook 📱. Ntabwo byari amagambo menshi… 💬 ariko byari bihagije 🤍.
 “Byee… I love u.” 💔💌
@@ -116,7 +116,7 @@ Ariko nanone… byari bitandukanye 🌙.
 Byari nk’aho turi abantu babiri bigeze kugira inkuru imwe 📖❤️, ariko ubu bayisoma batandukanye 📖💔.
 Njye ndacyayibuka… 😔🤍
 ndacyayibika… 💭💔
-kandi n’iyo nkubura mu magambo… uri mu mutima 💖🌙.`,
+kandi n’iyo nakubura mu magambo… uri mu mutima 💖🌙.`,
 bg: "https://res.cloudinary.com/dn0250gby/image/upload/v1775893734/img5_heblct.jpg",
 song: "https://res.cloudinary.com/dn0250gby/video/upload/v1775893836/song5_fgdpzh.mp3"
 },
@@ -125,13 +125,14 @@ song: "https://res.cloudinary.com/dn0250gby/video/upload/v1775893836/song5_fgdpz
 title: "Chapter 6: What I Never Showed You 🤍💭📖",
 text: `
 Hari ibintu byinshi utigeze ubona… 👀💔 si uko nabishakaga kuguhisha ❌🤍, ahubwo ni uko nari ntabizi gusobanura 💭😔.
-Ushobora kuba warabonaga ko ndi kure… 🚶‍♂️🌫️ ko ntakwitayeho 💔, ko wenda nagize abandi ❓… ariko ukuri ni uko nta na rimwe byigeze biba byo ❌❤️.
+Ushobora kuba warabonaga ko ndi kure… 🚶‍♂️🌫️ ko ntakwitayeho 💔, ko wenda nagize abandi ❓… ariko ukuri ni uko nta na rimwe byigeze bibaho ibyo ❌❤️.
 Nari mfite ibintu byinshi ku mutima… 💭🧠 regret yo kutaguha umwanya 😔, kwibaza niba narakubabaje kuruta uko nabyumvaga 💔, no kumva ko wenda natakaje umuntu nari mfite agaciro gakomeye mu buzima bwanjye 🤍🌙.
 N’uyu munsi, iyo mbonye status yawe kuri WhatsApp 📱💭… ndahagarara nkayireba 👀. Si ukubera ko nshaka kumenya gusa uko umeze… ahubwo ni uko hari igice cyanjye kigihari kuri wowe ❤️‍🩹.
-Hari igihe umutima wanjye ujya kure cyane 🌙💭… nkibaza uko byari kugenda iyo nza kugukorera ibyo wari ukwiye kubona icyo gihe 🤍✨.
+Hari nigihe kenshi njya muri keyboard ya chat yange nawe kuri Whatsapp nkashaka kukwandikira ariko imbaraga zikabura😭 kubera ko nagukoreye amakosa menshi 🛐 nkumva sinabona aho naguhera nkabyihorera nkagumana agahinda mumutima💔 kuko byose byatewe nange🥹
+Hari igihe umutima wanjye ujya kure cyane 🌙💭… nkibaza uko byari kugenda iyo nza kugukorera ibyo wari ukwiye kubona icyo gihe 🤍✨. 
 Ariko nanone, nkihangana 💪😔… nkibuka ko ibyo nakoze byose byari bifite impamvu 🧠💭, nubwo bitabashije gusobanuka uko byari bikwiye.
 Kandi mu mutima wanjye ❤️🌙, sinigeze nica isezerano naguhaye 🤝🤍. Nta wundi muntu wigeze aza gusimbura uwo mwanya ❌💔.
-Kuko kuri njye… byari wowe ❤️✨.`,
+Kuko kuri njye… igihe cyose yari wowe ❤️✨.`,
 bg: "https://res.cloudinary.com/dn0250gby/image/upload/v1775893726/img6_uxbjnl.jpg",
 song: "https://res.cloudinary.com/dn0250gby/video/upload/v1775893858/song6_nfjpus.mp3"
 },
@@ -140,7 +141,7 @@ song: "https://res.cloudinary.com/dn0250gby/video/upload/v1775893858/song6_nfjpu
 title: "Chapter 7: If There’s Still Something Left 🌙❤️📖",
 text: `
 Nzi neza ko ubuzima bushobora kuba bwarahinduye byinshi 🔄💭.
-Birashoboka ko wowe wamaze gukomeza 🚶‍♀️✨… ko hari abandi baguhaye umwanya 🤍, baguha ibyo wifuzaga icyo gihe 💖. Kandi niba ari byo, sinabiguhakana ❌🤍… kuko wari ubikwiye 🌙✨.
+Birashoboka ko wowe wamaze gukomeza 🚶‍♀️✨… ko hari abandi baguhaye umwanya 🤍, baguha ibyo wifuzaga icyo gihe 💖. Kandi niba ari byo, sinabikurenganyiriza ❌🤍… kuko wari ubikwiye 🌙✨.
 Nta na rimwe nakwifuriza ikindi kitari ibyiza 🌹🤍.
 Ariko nanone… hari ikintu ntashobora guceceka 💭❤️.
 Niba hari n’agace gato kagisigaye ✨, nubwo kaba ari gake gute 🤏🤍… kagifite ibyo twigeze gusangira 💭❤️, ndifuza ko wabona uwo ndi kuba we uyu munsi 🌱✨.
@@ -181,7 +182,7 @@ Ariko niba hari n’akanya gato uzafata ugasoma ibi byose… 📖💭
 Wibuke ko hari umuntu wigeze kukureba… 👀❤️
 akabona byinshi kurenza uko wabitekerezaga 🌙✨.
 Kandi n’uyu munsi… 🤍 akiri umuntu ushima ko mu buzima bwe… 💭🌙
-Yigeze kugira wowe ❤️✨
+Yigeze kugira wowe ❤️✨ byee wakoze kumpa umwanya wawe ibihe byiza👋🏾
 — DAMGI ❤️`,
 bg: "https://res.cloudinary.com/dn0250gby/image/upload/v1775893720/img9_mtetkc.jpg",
 song: "https://res.cloudinary.com/dn0250gby/video/upload/v1775893844/song9_hf7fjm.mp3"
